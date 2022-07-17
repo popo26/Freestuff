41173410ccc47893dc13c2c7f3923ecb5a22baec
popo26 <mynameispopo26@gmail.com>
2022-07-18 11:24:39 +1200

    a

--- app/__init__.py
+++ app/__init__.py
@@ -81,6 +81,7 @@ def create_app(config_name = "default"):
 
     search.create_index(update=True)
     
+    
     from .main import main as main_blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(main_blueprint)
